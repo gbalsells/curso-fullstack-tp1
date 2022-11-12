92a28230faaa73305db42d56b1088c6301b5ce7c
melinasaab <melinasaabaraoz@gmail.com>
2022-11-12 17:14:49 -0300

    Ultima modficacion del tp4

--- scripts/SaabMelina.js
+++ scripts/SaabMelina.js
@@ -1,22 +1,23 @@
-numero1 = 78
-numero2 = 23
+let numero1 = 78
+let numero2 = 23
 console.log(numero1)
 console.log(numero2)
 
-x = numero1+numero2
-y = numero1*numero2
-resultado1 = "La suma entre ambos números es " + x
-resultado2 = "El producto entre ambos números es " + y
-resultado3 = "El número" + " " + Math.max(numero1,numero2) + " " + "es el mayor"
+let x = numero1+numero2
+let y = numero1*numero2
+let resultado1 = "La suma entre ambos números es " + x
+let resultado2 = "El producto entre ambos números es " + y
+let resultado3 = "El número" + " " + Math.max(numero1,numero2) + " " + "es el mayor"
 console.log(resultado1) 
 console.log(resultado2)
 console.log(resultado3)
 
+let posicion = resultado2.lastIndexOf(' ') + 1
 console.log("La longitud de '", resultado1, "' es : " + resultado1.length)
-console.log("El producto de los numeros empieza en el indice : " + resultado2.lastIndexOf('1'))
+console.log("El producto de los numeros empieza en el indice : " + posicion)
 
-if(numero1 == resultado3.slice(resultado3.indexOf('7'), 2 + resultado3.indexOf('7'))){
+if(resultado3.includes(numero1.toString())){
     console.log("El numero 1 es mayor que numero 2")
 }
-    
+
 
