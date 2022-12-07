083ee4dff8c5a07328ec9c10cc072078243a5db4
Guido Balsells <guidobalsells@gmail.com>
2022-12-07 16:25:32 -0300

    tp5 guido

--- scripts/guidobalsells.js
+++ scripts/guidobalsells.js
@@ -1,1 +1,16 @@
-alert('Iniciándose en el Desarrollo Web Full Stack')
+function sendInfo(arr) {
+  let contactInfo = {}
+
+  let inputName = document.querySelector('#nombre').value
+  let inputPhone = document.querySelector('#phone').value
+  let inputMessage = document.querySelector('#mensaje').value
+
+  if ([inputName, inputPhone, inputMessage].includes('')) {
+    alert('Debe completar todos los campos')
+  } else {
+    contactInfo.nombre = inputName
+    contactInfo.phone = inputPhone
+    contactInfo.message = inputMessage
+    alert('Mensaje Enviado! Gracias por contactarte, ' + contactInfo.nombre)
+  }
+}
