b72b90501fbc9b4d5bf84258d3c1ac9a7ea227b3
Lucca Matienzo <12matienzo@gmail.com>
2023-01-09 11:55:33 -0300

    Trabajo Practico N4

---
+++ scripts/matienzolucca.js
@@ -0,0 +1,30 @@
+//Trabajo Práctico N4
+
+//Punto 1
+let numero1, numero2
+numero1 = 12
+numero2 = 23
+console.log("numero1 es: ",numero1)
+console.log("numero2 es: ",numero2)
+
+//Punto 2
+let resultado1, resultado2, resultado3
+resultado1 = "La suma entre ambos números es " + (numero1 + numero2)
+console.log(resultado1)
+resultado2 = "El producto entre ambos números es " + (numero1 * numero2)
+console.log(resultado2)
+if (numero1>numero2) {
+    resultado3 = "El número 1 es el mayor"
+} else {
+    resultado3 = "El número 2 es el mayor"
+}
+console.log(resultado3)
+
+//Punto 3
+console.log("La longitud de la string resultado1 es: " + resultado1.length)
+console.log("El indice de resultado2 donde comienza el resultado de la multiplicacion es: " + resultado2.indexOf(numero1*numero2))
+if(resultado3.includes("1")){
+   console.log("Numero 1 es mayor que numero 2")
+} else {
+    console.log("Numero 2 es mayor que numero 1")
+}
